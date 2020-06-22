e5080e412dd72c8ce94db901253c0c6d6f47ac7f
lektor925 <partizan925@mail.ru>
2020-06-22 16:21:51 +0300

    Day 3

--- job/database_script.py
+++ job/database_script.py
@@ -1,17 +1,22 @@
-from job.models import *
+from job.models import Company, Specialty, Vacancy
 
-from job.data import *
-
+from job.data import companies, specialties, jobs
 
 for item in companies:
-	Company.objects.create(name=item['title'])
+    Company.objects.create(name=item['title'])
 
 
 for item in specialties:
-	Specialty.objects.create(code=item['cat'], title=item['title'])
+    Specialty.objects.create(code=item['cat'], title=item['title'])
 
 
 for item in jobs:
-	company = Company.objects.get(name=item['company'])
-	specialty = Specialty.objects.get(code=item['cat'])
-	Vacancy.objects.create(title=item['title'], description=item['desc'], salary_min=item['salary_from'], salary_max=item['salary_to'], published_at=item['posted'], company=company, specialty=specialty)
+    company = Company.objects.get(name=item['company'])
+    specialty = Specialty.objects.get(code=item['cat'])
+    Vacancy.objects.create(title=item['title'],
+                           description=item['desc'],
+                           salary_min=item['salary_from'],
+                           salary_max=item['salary_to'],
+                           published_at=item['posted'],
+                           company=company,
+                           specialty=specialty)
--- job/views.py
+++ job/views.py
@@ -1,58 +1,56 @@
 from django.db.models import Count
-from django.shortcuts import render
 from django.views.generic import TemplateView, ListView, DetailView
 
 from job.models import Specialty, Company, Vacancy
 
 
 class MainView(TemplateView):
-	template_name = 'job/index.html'
+    template_name = 'job/index.html'
 
-	def get_context_data(self, **kwargs):
-		context = super(MainView, self).get_context_data(**kwargs)
-		context['specialty'] = Specialty.objects.annotate(Count('vacancies__specialty'))
-		context['company'] = Company.objects.annotate(Count('company__company'))
-		context['search_list'] = context['specialty'][:4]
-		return context
+    def get_context_data(self, **kwargs):
+        context = super(MainView, self).get_context_data(**kwargs)
+        context['specialty'] = Specialty.objects.annotate(Count('vacancies__specialty'))
+        context['company'] = Company.objects.annotate(Count('company__company'))
+        context['search_list'] = context['specialty'][:4]
+        return context
 
 
 class VacanciesAllView(ListView):
-	template_name = 'job/vacancies.html'
-	model = Vacancy
+    template_name = 'job/vacancies.html'
+    model = Vacancy
 
-	def get_context_data(self, *, object_list=None, **kwargs):
-		context = super(VacanciesAllView, self).get_context_data(**kwargs)
-		context['vacancy_list'] = Vacancy.objects.all()
-		return context
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(VacanciesAllView, self).get_context_data(**kwargs)
+        context['vacancy_list'] = Vacancy.objects.all()
+        return context
 
 
 class VacanciesCatView(ListView):
-	template_name = 'job/vacancies.html'
-	model = Vacancy
+    template_name = 'job/vacancies.html'
+    model = Vacancy
 
-	def get_context_data(self, *, object_list=None, **kwargs):
-		context = super(VacanciesCatView, self).get_context_data(**kwargs)
-		context['category'] = Specialty.objects.get(code=self.kwargs['code'])
-		context['vacancy_list'] = Vacancy.objects.filter(specialty__code=self.kwargs['code'])
-		return context
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(VacanciesCatView, self).get_context_data(**kwargs)
+        context['category'] = Specialty.objects.get(code=self.kwargs['code'])
+        context['vacancy_list'] = Vacancy.objects.filter(specialty__code=self.kwargs['code'])
+        return context
 
 
 class VacancyDetail(DetailView):
-	template_name = 'job/vacancy.html'
-	model = Vacancy
+    template_name = 'job/vacancy.html'
+    model = Vacancy
 
 
 class CompaniesAll(ListView):
-	model = Company
+    model = Company
 
 
 class CompanyDetail(ListView):
-	template_name = 'job/company.html'
-	model = Vacancy
+    template_name = 'job/company.html'
+    model = Vacancy
 
-	def get_context_data(self, *, object_list=None, **kwargs):
-		context = super(CompanyDetail, self).get_context_data(**kwargs)
-		context['company'] = Company.objects.get(id=self.kwargs['id'])
-		context['vacancy_list'] = Vacancy.objects.filter(company_id=self.kwargs['id'])
-		return context
-
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(CompanyDetail, self).get_context_data(**kwargs)
+        context['company'] = Company.objects.get(id=self.kwargs['id'])
+        context['vacancy_list'] = Vacancy.objects.filter(company_id=self.kwargs['id'])
+        return context
