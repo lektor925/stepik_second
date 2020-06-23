459e00b2660934b77dd9dcbeb09941636958d770
lektor925 <partizan925@mail.ru>
2020-06-23 11:26:25 +0300

    Day 4

--- job/views.py
+++ job/views.py
@@ -21,6 +21,7 @@ class VacanciesAllView(ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(VacanciesAllView, self).get_context_data(**kwargs)
+        context['page_title'] = 'Все вакансии'
         context['vacancy_list'] = Vacancy.objects.all()
         return context
 
@@ -28,12 +29,10 @@ class VacanciesAllView(ListView):
 class VacanciesCatView(ListView):
     template_name = 'job/vacancies.html'
     model = Vacancy
+    allow_empty = False
 
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super(VacanciesCatView, self).get_context_data(**kwargs)
-        context['category'] = Specialty.objects.get(code=self.kwargs['code'])
-        context['vacancy_list'] = Vacancy.objects.filter(specialty__code=self.kwargs['code'])
-        return context
+    def get_queryset(self):
+        return Vacancy.objects.filter(specialty__code=self.kwargs['code'])
 
 
 class VacancyDetail(DetailView):
@@ -48,9 +47,7 @@ class CompaniesAll(ListView):
 class CompanyDetail(ListView):
     template_name = 'job/company.html'
     model = Vacancy
+    allow_empty = False
 
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super(CompanyDetail, self).get_context_data(**kwargs)
-        context['company'] = Company.objects.get(id=self.kwargs['id'])
-        context['vacancy_list'] = Vacancy.objects.filter(company_id=self.kwargs['id'])
-        return context
+    def get_queryset(self):
+        return Vacancy.objects.filter(company_id=self.kwargs['id'])
