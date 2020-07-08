2267cdbcefbc31b81e69899d36782e0bd43fa37a
lektor925 <partizan925@mail.ru>
2020-07-08 08:10:19 +0300

    Day 13.

--- second_project/settings.py
+++ second_project/settings.py
@@ -135,7 +135,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'job/static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 LOGIN_REDIRECT_URL = '/'
