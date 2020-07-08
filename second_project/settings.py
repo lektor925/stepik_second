6ca90909699ace24b42dc89bac8a1468f77b1195
lektor925 <partizan925@mail.ru>
2020-07-08 07:48:21 +0300

    Day 13.

--- second_project/settings.py
+++ second_project/settings.py
@@ -126,13 +126,13 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-MEDIA_ROOT = 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 MEDIA_URL = '/media/'
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'job/static'),
