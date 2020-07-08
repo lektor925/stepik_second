a16d9f63ec77d53906620b2cc965cbe664aa828f
lektor925 <partizan925@mail.ru>
2020-07-08 08:12:14 +0300

    Day 13.

--- second_project/settings.py
+++ second_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'uczl!3k8f73=i&bm07^(oi7(!5e)_^wz431^j50kd(c81aa9v-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -135,7 +135,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'job/static'),
 ]
 
 LOGIN_REDIRECT_URL = '/'
